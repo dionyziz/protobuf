130eaff1a87833937d8df1ad862979db820abca2
Dionysis Zindros <dionyziz@gmail.com>
2015-01-08 14:03:41 +0100

    Add some dependencies that might be affecting our case

--- before_install.sh
+++ before_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get install python-protobuf libprotoc-dev protobuf-compiler m2crypto python-support python-pandas libdistorm64-1 libdistorm64-dev python-psutil pytsk3
+
 echo "Running autogen.sh..."
 # minimal testcase for https://github.com/google/protobuf/issues/149
 ./autogen.sh
