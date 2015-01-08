f35075ce312b4e75d772a8fa8f99ad2709505d72
Dionysis Zindros <dionyziz@gmail.com>
2015-01-08 13:58:35 +0100

    Run autogen twice to see if this will help

--- before_install.sh
+++ before_install.sh
@@ -2,6 +2,8 @@
 echo "Running autogen.sh..."
 # minimal testcase for https://github.com/google/protobuf/issues/149
 ./autogen.sh
+echo "Running autogen.sh again..."
+./autogen.sh
 echo "Running ./configure --prefix=/usr"
 ./configure --prefix=/usr
 echo "Running make"
