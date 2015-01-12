6b65048f3461897a8361c1c727d0d9d051950adf
Dionysis Zindros <dionyziz@gmail.com>
2015-01-12 16:22:30 +0100

    Run ./autogen once

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ pwd
 echo "Running autogen.sh..."
 # minimal testcase for https://github.com/google/protobuf/issues/149
 ./autogen.sh
-./autogen.sh
 echo "Running ./configure --prefix=/usr"
 ./configure --prefix=/usr
 echo "Running make"
